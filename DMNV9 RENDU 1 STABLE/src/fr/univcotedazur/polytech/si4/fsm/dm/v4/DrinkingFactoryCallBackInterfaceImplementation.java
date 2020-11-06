29829cab7a091edfbef6a877f8109bc59a9aafaf
RomainLamere <54950366+RomainLamere@users.noreply.github.com>
2020-11-06 12:16:11 +0100

    création de la v10 pour les futurs changement

---
+++ DMNV9 RENDU 1 STABLE/src/fr/univcotedazur/polytech/si4/fsm/dm/v4/DrinkingFactoryCallBackInterfaceImplementation.java
@@ -0,0 +1,31 @@
+package fr.univcotedazur.polytech.si4.fsm.dm.v4;
+
+import dmnv9.defaultsm.IDefaultSMStatemachine.SCInterfaceOperationCallback;
+
+public class DrinkingFactoryCallBackInterfaceImplementation implements SCInterfaceOperationCallback {
+
+	DrinkFactoryMachine theGui;
+    public DrinkingFactoryCallBackInterfaceImplementation(DrinkFactoryMachine sw) {theGui = sw; }
+	@Override
+	public String getSelection() {
+		return theGui.getSelection();
+	}
+	@Override
+	public boolean isHot() {
+		return theGui.isHot();
+	}
+	@Override
+	public boolean isPay() {
+		return theGui.isPay();
+	}
+	@Override
+	public boolean isPoor() {
+		return theGui.isPoor();
+	}
+	@Override
+	public boolean isTaken() {
+		return theGui.isTaken();
+	}
+
+
+}
